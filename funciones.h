f3acc6c762f7d1f89de43bad2ee4f024feacaf0a
valeria5c <125423564+valeria5c@users.noreply.github.com>
2025-11-18 13:17:31 -0500

    funciones.h

--- funciones.h
+++ funciones.h
@@ -2,11 +2,10 @@
 #define FUNCIONES_H
 
 // Se definen constantes globales para limitar tamaños del sistema
-#define MAX_PRODUCTOS 50      // Cantidad máxima de productos que se pueden registrar
-#define MAX_INSUMOS 50        // Cantidad máxima de insumos permitidos
-#define LEN_NOMBRE 50         // Tamaño máximo de los nombres de productos e insumos
+#define MAX_PRODUCTOS 5      // Cantidad máxima de productos que se pueden registrar
+#define MAX_INSUMOS 10        // Cantidad máxima de insumos permitidos
+#define LEN_NOMBRE 30         // Tamaño máximo de los nombres de productos e insumos
 
-// PROTOTIPOS DE FUNCIONES
 
 // Muestra el menú principal y retorna la opción seleccionada
 int menu();
@@ -26,14 +25,14 @@ char ValidacionLetras(const char mensaje[]);
 // Verifica que exista al menos un producto antes de ciertas operaciones
 int ValidacionProdExist(int cont);
 
-// Busca un producto o insumo por nombre (sin importar mayúsc/minúsc)
-// Retorna el índice donde se encuentra o -1 si no existe
+// Busca un producto o insumo por nombre (sin importar mayúsculas/minúsculas)
+// Retorna el índice donde se encuentra el producto o insumo o -1 si no existe
 int buscarProducto(char *lista[], int total, char buscado[]);
 
 // Registro de insumos del inventario inicial
 int registrarInsumos(char *nom_insumos[], int inventario[], int totalInsumos);
 
-// Registro de nuevos productos con sus tiempos e insumos asociados
+// Registro de nuevos productos con sus tiempos e insumos correspondientes
 int registrarProductosSistema(
     char *nom_prod[],
     int tiempo[],
@@ -44,13 +43,10 @@ int registrarProductosSistema(
     int totalProd);
 
 // Edición de productos ya registrados
-void editarProducto(char *nom_prod[],
-                    int tiempo[],
+void editarProducto(char *nom_prod[], int tiempo[],
                     int CantInsumosPorProducto[][MAX_INSUMOS],
-                    char *nom_insumos[],
-                    int inventario[],
-                    int totalProd,
-                    int totalInsumos);
+                    char *nom_insumos[], int inventario[],
+                    int totalProd, int *totalInsumos);
 
 // Eliminación de productos (reduce cantidad total)
 int eliminarProducto(char *nom_prod[],
@@ -58,7 +54,7 @@ int eliminarProducto(char *nom_prod[],
                      int CantInsumosPorProducto[][MAX_INSUMOS],
                      int totalProd);
 
-// Mostrar todos los productos registrados con sus insumos y tiempos
+// Mostrar todos los productos registrados con sus insumos y tiempos de producción
 void mostrarProductos(char *nom_prod[],
                       int tiempo[],
                       int CantInsumosPorProducto[][MAX_INSUMOS],
